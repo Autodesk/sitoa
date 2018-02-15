11278823fc3f5a1f61ff164aaad72741c44748bc
Frederic Servant <fred@solidangle.com>
2018-02-15 21:46:27 +0000

    Pimp up default config files

--- config/custom_windows.py
+++ config/custom_windows.py
@@ -1,11 +1,18 @@
-TARGET_ARCH = 'x86_64'
+# Sample configuration file for Windows
+#
+# Copy this file as 'custom.py' at the root of the repo if you need to customize
+# the build variables
 
+TARGET_ARCH         = 'x86_64'
 MSVC_VERSION        = '11.0'
 VS_HOME             = r'C:/Program Files (x86)/Microsoft Visual Studio 11.0/VC'
-VS_KIT              = r'C:/Program Files (x86)/Windows Kits/8.0'
+WINDOWS_KIT         = r'C:/Program Files (x86)/Windows Kits/8.0'
+
 XSISDK_ROOT         = r'C:/Program Files/Autodesk/Softimage 2015/XSISDK'
 ARNOLD_HOME         = r'C:/SolidAngle/Arnold-5.0.1.1/win64'
-TARGET_WORKGROUP_PATH  = r'./Softimage_2015/Addons/SItoA'
-MODE = 'opt'
-SHOW_CMDS = 'True'
 
+TARGET_WORKGROUP_PATH  = r'./Softimage_2015/Addons/SItoA'
+
+WARN_LEVEL = 'strict'
+MODE       = 'opt'
+SHOW_CMDS  = True
