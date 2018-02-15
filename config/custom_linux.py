9a581bd3b541165e3573d617e4a52026d47ec443
Frederic Servant <fred@solidangle.com>
2018-02-15 21:46:27 +0000

    Pimp up default config files

--- config/custom_linux.py
+++ config/custom_linux.py
@@ -1,8 +1,15 @@
-WARN_LEVEL  = 'warn-only'
-SHCXX       = r'/use/bin/gcc-4.2.4/bin/gcc-4.2.4'
+# Sample configuration file for Linux
+#
+# Copy this file as 'custom.py' at the root of the repo if you need to customize
+# the build variables
+
+SHCXX       = r'/usr/bin/gcc-4.2.4/bin/gcc-4.2.4'
+
 XSISDK_ROOT = r'/usr/Softimage/Softimage_2015/XSISDK'
 ARNOLD_HOME = r'/usr/SolidAngle/Arnold-5.0.1.1/linux'
-TARGET_WORKGROUP_PATH = './Softimage_2015/Addons/SItoA'
-MODE        = 'opt'
-SHOW_CMDS   = 'True'
 
+TARGET_WORKGROUP_PATH = './Softimage_2015/Addons/SItoA'
+
+WARN_LEVEL  = 'warn-only' # lots of warnings are expected on Linux
+MODE        = 'opt'
+SHOW_CMDS   = True
