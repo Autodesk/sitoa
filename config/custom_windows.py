1c1ae2177b31aeb1331cf3bcaf7fde75f30dacdc
Jens Lindgren <lindgren_jens@hotmail.com>
2019-06-30 00:08:29 +0200

    bump min arnold version to 5.3.1.1

--- config/custom_windows.py
+++ config/custom_windows.py
@@ -9,7 +9,7 @@ VS_HOME             = r'C:/Program Files (x86)/Microsoft Visual Studio 11.0/VC'
 WINDOWS_KIT         = r'C:/Program Files (x86)/Windows Kits/8.0'
 
 XSISDK_ROOT         = r'C:/Program Files/Autodesk/Softimage 2015/XSISDK'
-ARNOLD_HOME         = r'C:/SolidAngle/Arnold-5.3.1.0/win64'
+ARNOLD_HOME         = r'C:/SolidAngle/Arnold-5.3.1.1/win64'
 
 TARGET_WORKGROUP_PATH  = r'./Softimage_2015/Addons/SItoA'
 
