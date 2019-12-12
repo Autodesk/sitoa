6066a053f72f0b0f83fda9f55b0b2d65f055f95a
Jens Lindgren <lindgren_jens@hotmail.com>
2019-12-13 00:51:50 +0100

    switch to arnold 6 and bump sitoa version

--- plugins/sitoa/version.cpp
+++ plugins/sitoa/version.cpp
@@ -13,8 +13,8 @@ See the License for the specific language governing permissions and limitations 
 
 #include <xsi_utils.h>
 
-#define SITOA_MAJOR_VERSION_NUM    5
-#define SITOA_MINOR_VERSION_NUM    4
+#define SITOA_MAJOR_VERSION_NUM    6
+#define SITOA_MINOR_VERSION_NUM    0
 #define SITOA_FIX_VERSION          L"0"
 
 
