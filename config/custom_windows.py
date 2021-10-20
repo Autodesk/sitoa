9a5cbd8c64c01a9b7b6c1fde69dce6d640a416da
Jens Lindgren <lindgren_jens@hotmail.com>
2021-10-21 00:37:03 +0200

    bump minimum arnold version

--- config/custom_windows.py
+++ config/custom_windows.py
@@ -9,7 +9,7 @@ VS_HOME             = r'C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC'
 WINDOWS_KIT         = r'C:/Program Files (x86)/Windows Kits/10'
 
 XSISDK_ROOT         = r'C:/Program Files/Autodesk/Softimage 2015 SP2/XSISDK'
-ARNOLD_HOME         = r'C:/SolidAngle/Arnold-6.2.1.0/win64'
+ARNOLD_HOME         = r'C:/SolidAngle/Arnold-7.0.0.0-windows'
 
 TARGET_WORKGROUP_PATH  = r'./dist/Softimage_2015/Addons/SItoA'
 
